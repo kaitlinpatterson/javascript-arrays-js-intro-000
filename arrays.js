fae4e89f6902139a59a8df8eb2588aa122cee3b1
Kaitlin Patterson <kaitlin.patterson@observepoint.com>
2019-07-30 19:40:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,7 +24,7 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
- return array.unshift("element") array
+ return array.unshift("element") 
 }
 function addElementToEndOfArray(array, element) {
   return [...array, element]
