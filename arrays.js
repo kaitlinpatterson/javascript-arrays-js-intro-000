bd0bea988463c5c4a73e5b511ec9464757c8b423
Kaitlin Patterson <kaitlin.patterson@observepoint.com>
2019-07-30 19:02:47 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var grilledCheeseIngredients = [
+  'bread',
+  'mild cheese',
+  'butter',
+  'tomato',
+  'garlic',
+]
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+]
+var chocolateBars = ['snickekrs', 'hundred grand', 'kitkat', 'skittles']
+var cities = ["New York", "San Francisco"]
