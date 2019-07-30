0f3b79da792f433bf35618efbb253d44998001e8
Kaitlin Patterson <kaitlin.patterson@observepoint.com>
2019-07-30 19:26:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -36,7 +36,7 @@ function accessElementInArray(array,index) {
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift()
+  return array.shift(3)
 }
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
