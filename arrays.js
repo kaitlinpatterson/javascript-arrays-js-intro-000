34e9918983aa27d7a17f6497b6c24af75e22f5fc
Kaitlin Patterson <kaitlin.patterson@observepoint.com>
2019-07-30 19:17:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,12 +24,14 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+ return array.unshift(element)
 }
 function addElementToEndOfArray(array, element) {
   return [...array, element]
 }
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element);
+ return array.push(element);
 }
-function accessElementInArray(array,index)
+function accessElementInArray(array,index) {
+  console.log(array[index])
+}
